745a060338e7880c10b21e039015153b31d88513
Jun D. Ouyang <jdouyang@ucdavis.edu>
2013-11-26 12:24:04 +0800

    Modified Win32 batch file. Updated logs + README.

--- run.bat
+++ run.bat
@@ -1,12 +1,11 @@
 @echo off
-
-hspice nrs.cir > nrs.log
-hspice TestPreemp.cir > TestPreemp.log
-hspice TestDeemp.cir > TestDeemp.log
-
+hspice nrs.cir > nrs.log 2>nul
+hspice TestPreemp.cir > TestPreemp.log 2>nul
+hspice TestDeemp.cir > TestDeemp.log 2>nul
 type preamble > README.md
-echo ‘’
-echo ‘##Lastest Output’
-echo ‘```’ >> README.md
+echo. >> README.md
+echo. >> README.md
+echo ##Lastest Output >> README.md
+echo ``` >> README.md
 type nrs.log >> README.md
-echo ‘```’ >> README.md
+echo ``` >> README.md
