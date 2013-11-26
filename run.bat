c27b4d796605668050c8019b9a4d3ab11728e1ab
jun <jouyang3@mail.ccsf.edu>
2013-11-25 19:50:56 -0800

    Fixed bugs in 'ends' of gain.cir. Allowed uploadable hspice logs.

--- run.bat
+++ run.bat
@@ -3,3 +3,10 @@
 hspice nrs.cir > nrs.log
 hspice TestPreemp.cir > TestPreemp.log
 hspice TestDeemp.cir > TestDeemp.log
+
+type preamble > README.md
+echo ‘’
+echo ‘##Lastest Output’
+echo ‘```’ >> README.md
+type nrs.log >> README.md
+echo ‘```’ >> README.md
--- run.sh
+++ run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 hspice nrs.cir > nrs.log
-hspice TestPreemp > TestPreemp.log
-hspice TestDeemp > TestDeemp.log
+hspice TestPreemp.cir > TestPreemp.log
+hspice TestDeemp.cir > TestDeemp.log
+
+cat preamble > README.md
+echo ‘’
+echo ‘##Lastest Output’
+echo ‘```’ >> README.md
+cat nrs.log >> README.md
+echo ‘```’ >> README.md
